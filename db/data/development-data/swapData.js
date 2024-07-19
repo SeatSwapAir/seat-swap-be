a9d3575af2f4f1141aa86b20d5c8122d4475e554
Rahul Mulchandani <44975642+mulcharv@users.noreply.github.com>
2024-07-19 16:21:54 +0100

    feat: swapData introduced

---
+++ db/data/development-data/swapData.js
@@ -0,0 +1,30 @@
+module.exports = [
+  {
+    offered_seat_id: 453,
+    requested_seat_id: 452,
+    swap_request_date: 1721392631,
+    swap_approval_date: null,
+    rejection: false,
+  },
+  {
+    offered_seat_id: 453,
+    requested_seat_id: 455,
+    swap_request_date: 1721392631,
+    swap_approval_date: null,
+    rejection: true,
+  },
+  {
+    offered_seat_id: 451,
+    requested_seat_id: 453,
+    swap_request_date: 1721392631,
+    swap_approval_date: null,
+    rejection: false,
+  },
+  {
+    offered_seat_id: 450,
+    requested_seat_id: 453,
+    swap_request_date: 1721392631,
+    swap_approval_date: null,
+    rejection: true,
+  },
+];
---
+++ db/data/test-data/swapData.js
@@ -0,0 +1,30 @@
+module.exports = [
+  {
+    offered_seat_id: 453,
+    requested_seat_id: 452,
+    swap_request_date: '2024-07-19 15:12:43.790189+01',
+    swap_approval_date: null,
+    rejection: false,
+  },
+  {
+    offered_seat_id: 453,
+    requested_seat_id: 455,
+    swap_request_date: '2024-07-19 15:12:43.790189+01',
+    swap_approval_date: null,
+    rejection: true,
+  },
+  {
+    offered_seat_id: 451,
+    requested_seat_id: 453,
+    swap_request_date: '2024-07-19 15:12:43.790189+01',
+    swap_approval_date: null,
+    rejection: false,
+  },
+  {
+    offered_seat_id: 450,
+    requested_seat_id: 453,
+    swap_request_date: '2024-07-19 15:12:43.790189+01',
+    swap_approval_date: null,
+    rejection: true,
+  },
+];
